52c72f2e1897e94c49327c1374077684de066dc8
ilya moskovko <ilya@iswifter.net>
2014-09-09 23:53:18 -0700

    added angular ui-router

--- app/app.js
+++ app/app.js
@@ -2,11 +2,19 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  'ui.router'
+])
+.config(function ($stateProvider) {
+  $stateProvider
+    .state('index', {
+      url: '',
+      views: {
+        'viewA': {
+          template: 'viewA'
+        },
+        'viewB': {
+          template: 'viewB'
+        }
+      }
+    });
+});
