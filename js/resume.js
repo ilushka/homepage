8b5c4831737d5e95a9d4af6bd8f5882e3df776ba
ilya moskovko <ilya@iswifter.net>
2017-01-20 16:05:43 -0800

    ** added throttling and debouncing **

--- js/resume.js
+++ js/resume.js
@@ -1,5 +1,5 @@
 function AnimeObj(obj) {
-    this.obj = obj;     // object to animate
+    this.obj = obj;                 // object to animate
     this.animeInProgress = false;   // is animation in progress
     this.rate = 1;                  // unit count that is used to modify property per each frame 
 
@@ -31,17 +31,41 @@ function AnimeObj(obj) {
         }
     };
 }
+/* execute function every N events */
+var throttleFunc = function(rate, func) {
+    var eventCount = 0;
+    return function(e) {
+        if (eventCount === 0) {
+            func();
+            eventCount = rate;
+        }
+        eventCount--;
+    }
+};
+/* execute function every N milliseconds */
+var debounceFunc = function(interval, func) {
+    var okToExec = true;
+    return function(e) {
+        if (okToExec === true) {
+            okToExec = false;
+            func();
+            setTimeout(function() {okToExec = true;}, interval);
+        }
+    }
+};
 
 window.addEventListener("load", function(event) {
     var popupAnime = new AnimeObj(document.getElementById("popup"));
     popupAnime.setRate(18);
-    var animatePopup = function() {
+    var scrollPopup = function() {
         popupAnime.animateY(window.innerHeight, 30);
     };
+    var scrollPopupDebounced = debounceFunc(500, scrollPopup);
 
     window.onmousewheel = window.onwheel = window.ontouchmove = document.onkeydown = function(e) {
         e.preventDefault();
         console.log(e);
-        animatePopup();
+        scrollPopupDebounced(e);
     };
 }, false);
+
